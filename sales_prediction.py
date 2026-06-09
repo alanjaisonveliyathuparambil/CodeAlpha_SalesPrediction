9f5a68720f04bf8aae9e258e079de5e58f44b45c
Alan Jaison <alanjaison144@gmail.com>
2026-06-09 10:44:48 +0530

    Final sales prediction project

--- sales_prediction.py
+++ sales_prediction.py
@@ -1,16 +1,56 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 df = pd.read_csv("Advertising.csv")
+
 df = df.drop("Unnamed: 0", axis=1)
+
+print("First 5 Rows:")
+print(df.head())
+
+print("\nDataset Shape:")
+print(df.shape)
+
+print("\nMissing Values:")
+print(df.isnull().sum())
+
+print("\nStatistical Summary:")
+print(df.describe())
+
+# Graph 1 : TV vs Sales
+
+plt.figure(figsize=(8,5))
+plt.scatter(df["TV"], df["Sales"])
+
+plt.title("TV Advertising vs Sales")
+plt.xlabel("TV Advertising Budget")
+plt.ylabel("Sales")
+
+plt.tight_layout()
+plt.show()
+
+# Graph 2 : Correlation Heatmap
+
+plt.figure(figsize=(8,6))
+sns.heatmap(df.corr(), annot=True, cmap="Blues")
+
+plt.title("Correlation Heatmap")
+
+plt.tight_layout()
+plt.show()
+
 
 X = df[["TV", "Radio", "Newspaper"]]
 y = df["Sales"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y,
+    X,
+    y,
     test_size=0.2,
     random_state=42
 )
@@ -19,6 +59,15 @@ model = LinearRegression()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
+
+
+print("\nModel Performance")
+
+print("MAE :", mean_absolute_error(y_test, y_pred))
+print("MSE :", mean_squared_error(y_test, y_pred))
+print("R2 Score :", r2_score(y_test, y_pred))
+
+# Graph 3 : Actual vs Predicted
 
 plt.figure(figsize=(8,5))
 plt.scatter(y_test, y_pred)
